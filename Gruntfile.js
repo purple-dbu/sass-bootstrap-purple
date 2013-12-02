d4882b484bb972a512e304ed7c3c98c99d9567d3
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-12-02 17:21:32 +0100

    Replace grunt-contrbi-compass by grunt-contrib-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,31 +19,32 @@ module.exports = function(grunt) {
       dist: ['dist']
     },
 
-    compass: {
+    sass: {
       options: {
-        sassDir: 'scss',
-        cssDir: 'dist',
-        imagesDir: 'images',
-        javascriptsDir: 'scripts',
-        fontsDir: 'fonts',
-        importPath: 'bower_components',
-        httpImagesPath: '../images',
-        httpFontsPath: '../fonts',
-        relativeAssets: true,
-        assetCacheBuster: false,
-        banner: '<%= banner %>'/*,
-        raw: 'sass_options = {:sourcemap => true}'*/
+        banner: '<%= banner %>',
+        unixNewlines: true,
+        loadPath: [
+          'bower_components'
+        ]
       },
       development: {
         options: {
-          environment: 'development',
-          specify: 'scss/theme-purple.scss'
+          style: 'expanded',
+          debugInfo: true,
+          lineNumbers: true,
+          sourcemap: true
+        },
+        files: {
+          'dist/bootstrap-theme-purple.css': 'scss/theme-purple.scss'
         }
       },
       production: {
         options: {
-          environment: 'production',
-          specify: 'scss/theme-purple.min.scss'
+          style: 'compressed',
+          banner: '<%= banner %>'
+        },
+        files: {
+          'dist/bootstrap-theme-purple.min.css': 'scss/theme-purple.scss'
         }
       }
     },
@@ -57,7 +58,7 @@ module.exports = function(grunt) {
     },
 
     watch: {
-      compass: {
+      sass: {
         files: 'scss/*.scss',
         tasks: ['dist-css']
       }
@@ -67,12 +68,12 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // CSS distribution task.
-  grunt.registerTask('dist-css', ['compass:development', 'compass:production']);
+  grunt.registerTask('dist-css', ['sass:development', 'sass:production']);
 
   // Fonts distribution task.
   grunt.registerTask('dist-fonts', ['copy']);
